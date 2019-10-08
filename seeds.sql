3c952abdc82a621204d5ab3e7b1112f2cf168eea
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-10-07 17:00:08 -0700

    added js and sql files

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO products ( product_name,  department_name, price, stock_quantity)
+VALUES ("corgis", "pets", 3000.00, 10),
+("pugs", "pets", 2700.00, 10), 
+("llama", "pets", 8000.00, 1), 
+("penguins", "pets", 10.00, 5);
